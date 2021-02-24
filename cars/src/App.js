1c54f3311a27e79526af9303128d01a798c13e0f
codwerk <louis_widdy@live.fr>
2021-02-25 00:02:35 +0100

    DOM Events et changement de State P1

--- cars/src/App.js
+++ cars/src/App.js
@@ -7,11 +7,47 @@ class App extends Component {
   state = {
     titre : "Mon catalogue voitures !"
   }
+
+  changeTitre = (e) => {
+    this.setState({
+      titre: 'mon titre test !'
+    })
+  }
+
+  changeTitreViaParam = (titre) => {
+    this.setState({
+      titre: titre
+    })
+  }
+
+  changeTitreViaBind = (titre) => {
+    this.setState({
+      titre: titre
+    })
+  }
+
+  changeViaInput = (e) => {
+    this.setState({
+      titre: e.target.value
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <Mycars title= {this.state.titre}/>
+
+          <button onClick={this.changeTitre}>Changer le titre</button>
+
+          <button onClick={()=> this.changeTitreViaParam('mon titre test ! Via Param')}>Changer le titre Via Param</button>
+
+          <button onClick={this.changeTitreViaBind.bind(this, 'mon titre test ! Via Bind')}>Changer le titre Via Bind</button>
+
+          <br/>
+
+          <input type="text" onChange={this.changeViaInput} value={this.state.titre}/>
+
         </header>
       </div>
     );
--- cars/src/components/Mycars.js
+++ cars/src/components/Mycars.js
@@ -2,11 +2,24 @@ import React, { Component } from 'react';
 import Car from './Car';
 
 class Mycars extends Component {
+
+  noCopy = () => {
+    alert('ne pas copier le text !!!');
+  }
+
+  addClass = (e) => {
+    e.target.classList.toggle('red')
+  }
+
+
   render() {
     return (
     
       <div>
-        <h1>{ this.props.title }</h1>
+        <h1 onMouseOver={this.addClass}>{ this.props.title }</h1>
+
+        <p onCopy={this.noCopy}>ne pas copier le text !!!</p>
+        
         <Car color="blue"> Peugeot </Car>
         <Car> Mercedes </Car>
         <Car color="red"></Car>
