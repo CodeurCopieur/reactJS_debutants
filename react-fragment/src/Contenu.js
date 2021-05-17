c00763c3a943c6023147a566fd9349b4ab1c041d
codwerk <louis_widdy@live.fr>
2021-05-17 22:02:59 +0200

    Les React Fragment

---
+++ react-fragment/src/Contenu.js
@@ -0,0 +1,10 @@
+import {Fragment} from 'react'
+
+export default function Contenu() {
+  return (
+    <Fragment>
+    <h1>Hello, c'est contenu !!!</h1>
+    <p>description</p>
+    </Fragment>
+  )
+}
