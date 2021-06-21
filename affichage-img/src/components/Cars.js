8ce347bd4967760a312984e8865cbdda51b2c6d8
codwerk <louisws@outlook.fr>
2021-06-21 23:09:16 +0200

    Affichage des images dans React

---
+++ affichage-img/src/components/Cars.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import cars from '../cars.png'
+
+export default function Cars({ width, height}) {
+  return (
+
+      <img src={cars} width={width} height={height} alt=""/>
+
+  )
+}
---
+++ affichage-img/src/components/Form.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Carsvg from './Cars';
+
+export default class Form extends Component {
+  render() {
+    return (
+      <div>
+        <Carsvg  width="306" height="306"/>
+        <h1>Commentaire</h1>
+        Formulaire
+      </div>
+    )
+  }
+}
