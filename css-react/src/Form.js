913ac7ac39d764ee9841705ae95ba484b6c11ddc
codwerk <louisws@outlook.fr>
2021-07-14 14:29:13 +0200

    dynamiser les classes

--- css-react/src/Form.js
+++ css-react/src/Form.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
-import '../myCss.css'
-
+import React, { Component } from 'react';
+import MyHead from './myHeaderOne';
+import './myCss.css';
+import styles from './myCss.module.css'; // cibler le composant que nous voulons styliser
 
 const titreBleu = {
   fontSize: '50px',
@@ -15,11 +16,15 @@ export default class Form extends Component {
 
     return (
       <div>
+      <h1 className={styles.green}> Title green</h1>
+        <MyHead/>
         <p style={{fontSize: '50px', color: 'red'}}>Style Inline 1</p>
         <p style={titreBleu}>Style Inline 2</p>
         <p className="purple">Style w class</p>
 
-        <p className={myClass}>Je suis blue ou gris</p>
+        <p className={myClass + ' bigFont'}>Je suis blue ou gris</p>
+        <p className={`${myClass} bigFont`}>Je suis blue ou gris 2</p>
+        
         <button>Valider</button>
       </div>
     )
---
+++ css-react/src/myHeaderOne.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function myHeaderOne() {
+  return (
+    <header>
+      <span>Logo</span>
+      <ul>
+        <li>link1</li>
+        <li>link2</li>
+        <li>link3</li>
+        <li>link4</li>
+      </ul>
+    </header>
+  )
+}
