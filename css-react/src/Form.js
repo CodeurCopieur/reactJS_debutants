ef1002d8547d78f3fadc86a27c08dd20a43f1937
codwerk <louisws@outlook.fr>
2021-07-14 18:16:04 +0200

    use boostrap, styled-components

--- css-react/src/Form.js
+++ css-react/src/Form.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import MyHead from './myHeaderOne';
 import './myCss.css';
 import styles from './myCss.module.css'; // cibler le composant que nous voulons styliser
+import styled from 'styled-components';
 
 const titreBleu = {
   fontSize: '50px',
   color: 'blue'
 }
+
+const Title = styled.h2`
+color: purple;
+font-size: 80px;
+`;
+
+const Btn = styled.button`
+padding: 7px;
+background-color: purple;
+border-radius: 5px;
+border: 0;
+color: white;
+margin-bottom:20px;
+`
 
 export default class Form extends Component {
 
@@ -16,7 +31,14 @@ export default class Form extends Component {
 
     return (
       <div>
-      <h1 className={styles.green}> Title green</h1>
+        <Title>
+          styled-components
+        </Title>
+        <Btn>
+          Btn styled-components
+        </Btn>
+        
+        <h1 className={styles.green}> Title green</h1>
         <MyHead/>
         <p style={{fontSize: '50px', color: 'red'}}>Style Inline 1</p>
         <p style={titreBleu}>Style Inline 2</p>
@@ -25,7 +47,8 @@ export default class Form extends Component {
         <p className={myClass + ' bigFont'}>Je suis blue ou gris</p>
         <p className={`${myClass} bigFont`}>Je suis blue ou gris 2</p>
         
-        <button>Valider</button>
+        <button className="btn btn-danger">Btn style boostrap</button>
+
       </div>
     )
   }
